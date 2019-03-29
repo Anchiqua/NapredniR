6e5fe63a5343c87cc058dce99a1e66c87457ed05
tajadebeljak <taja.debeljak95@gmail.com>
2019-03-29 10:03:32 +0100

    sprememba postavitve

--- Ui.R
+++ Ui.R
@@ -3,58 +3,68 @@ library(shinythemes)
 # define the user interface
 shinyUI(fluidPage(theme=shinytheme("superhero"),
                   # define type of page layout
-                  pageWithSidebar(
+                  
                     
                     # define content of page header ####
-                    headerPanel("Poroke"),
+                    titlePanel("Poroke"),
                     
-                    # define content of left side of the page ####
-                    sidebarPanel(
-                      selectInput(inputId = "meritev", 
-                                  label = "Te zanima kako se skozi leta spreminja povprečna starost ali pa število sklenitev zakonskih zvez?",
-                                  choices = c("Povprečna starost ženina"="povp_starost_zenina",
-                                              "Povprečna starost neveste" = "povp_starost_neveste", 
-                                              "Število sklenitev zakonskih zvez" = "sklenitev_zvez", 
-                                              "Sklenitev zakonskih zvez na 1000 prebivalcev" = "sklenitev_zvez_na1000_prebiv"),
-                                  selected = "Povprečna starost neveste"),
-                      selectInput(inputId = "leta",
-                                  label = "Leta",
-                                  choices = c("1995" = "X1995",
-                                              "1996" = "X1996",
-                                              "1997" = "X1997",
-                                              "1998" = "X1998",
-                                              "1999" = "X1999",
-                                              "2000" = "X2000",
-                                              "2001" = "X2001",
-                                              "2002" = "X2002",
-                                              "2003" = "X2003",
-                                              "2004" = "X2004",
-                                              "2005" = "X2005",
-                                              "2006" = "X2006",
-                                              "2007" = "X2007",
-                                              "2008" = "X2008",
-                                              "2009" = "X2009",
-                                              "2010" = "X2010",
-                                              "2011" = "X2011",
-                                              "2012" = "X2012",
-                                              "2013" = "X2013",
-                                              "2014" = "X2014",
-                                              "2015" = "X2015",
-                                              "2016" = "X2016",
-                                              "2017" = "X2017"),
-                                  selected = "1995")
-                                 
-                    ),
+                    # define content of left side of the page
+                    sidebarLayout(
+                      sidebarPanel(div("Pozdravljeni v aplikaciji porok"), style="color:aquamarine", width=2), 
                     
-                    # define content of the main part of the page ####   
-                    mainPanel(
-                      tabsetPanel(
-                        tabPanel(title="Statistika porok skozi leta",      
-                                 plotOutput("plotPorokePoLetih")
-                        ),
-                        tabPanel(title="Poroke po regijah",
-                                 plotOutput("plotPorokePoRegijah"))
-                      )
+                      mainPanel(
+                        tabsetPanel(
+                          
+                          tabPanel("Poroke skozi leta",
+                                   sidebarPanel(
+                        
+                                    selectInput(inputId = "meritev", 
+                                                label = "Te zanima kako se skozi leta spreminja povprečna starost ali pa število sklenitev zakonskih zvez?",
+                                                choices = c("Povprečna starost ženina"="povp_starost_zenina",
+                                                            "Povprečna starost neveste" = "povp_starost_neveste", 
+                                                            "Število sklenitev zakonskih zvez" = "sklenitev_zvez", 
+                                                            "Sklenitev zakonskih zvez na 1000 prebivalcev" = "sklenitev_zvez_na1000_prebiv"),
+                                                selected = "Povprečna starost neveste")),
+                                   mainPanel(
+                                    plotOutput("plotPorokePoLetih")
+                                       )),
+                          
+                          tabPanel("Zemljevid", 
+                                   sidebarPanel(
+                                     selectInput(inputId = "leta",
+                                                 label = "Leta",
+                                                 choices = c("1995" = "X1995",
+                                                             "1996" = "X1996",
+                                                             "1997" = "X1997",
+                                                             "1998" = "X1998",
+                                                             "1999" = "X1999",
+                                                             "2000" = "X2000",
+                                                             "2001" = "X2001",
+                                                             "2002" = "X2002",
+                                                             "2003" = "X2003",
+                                                             "2004" = "X2004",
+                                                             "2005" = "X2005",
+                                                             "2006" = "X2006",
+                                                             "2007" = "X2007",
+                                                             "2008" = "X2008",
+                                                             "2009" = "X2009",
+                                                             "2010" = "X2010",
+                                                             "2011" = "X2011",
+                                                             "2012" = "X2012",
+                                                             "2013" = "X2013",
+                                                             "2014" = "X2014",
+                                                             "2015" = "X2015",
+                                                             "2016" = "X2016",
+                                                             "2017" = "X2017"),
+                                                 selected = "1995")),
+                                   mainPanel(
+                                     plotOutput("plotPorokePoRegijah")
+                                   )
+                        )
+                                       
+                                   
                     )
-                  )
-) )
+                  
+                   
+                  
+) )))
